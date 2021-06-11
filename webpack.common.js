68bb01164d091a1bcb8fb33c82636af14f8f7554
BassOfBass <vinniethepooloo@protonmail.com>
2021-06-12 01:37:49 +0500

    separated by pages

--- webpack.common.js
+++ webpack.common.js
@@ -18,15 +18,67 @@ const webpackConfig = {
       new RegExp(`[/\\\\\](${supportedLanguages.join('|')})[/\\\\\]index\.js$`)
     ),
     new HTMLWebpackPlugin({
+      filename: 'areas/index.html',
+      template: "./src/pages/areas/areas.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'addenda.html',
+      template: "./src/pages/addenda.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'car.html',
+      template: "./src/pages/car.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'character-design.html',
+      template: "./src/pages/character-design.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'combat.html',
+      template: "./src/pages/combat.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'companions.html',
+      template: "./src/pages/companions.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'encounters.html',
+      template: "./src/pages/encounters.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'endings.html',
+      template: "./src/pages/endings.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
       filename: 'index.html',
       template: "./src/pages/index.pug",
       chunks: ['index'],
     }),
     new HTMLWebpackPlugin({
-      filename: 'areas/index.html',
-      template: "./src/pages/areas/areas.pug",
+      filename: 'items.html',
+      template: "./src/pages/items.pug",
       chunks: ['index'],
     }),
+    new HTMLWebpackPlugin({
+      filename: 'preparations.html',
+      template: "./src/pages/preparations.pug",
+      chunks: ['index'],
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'stupid.html',
+      template: "./src/pages/stupid.pug",
+      chunks: ['index'],
+    }),
+    
+    
     new CopyWebpackPlugin({
       patterns: [
         { from: path.resolve(__dirname, "public") }
